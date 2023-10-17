306199591970ed0509deaac22e49d11273527762
Clayton Alves <claytonjuniorsos@gmail.com>
2023-10-17 14:55:17 -0300

    calculadora

---
+++ aula 1/main.js
@@ -0,0 +1,31 @@
+/* let nome = false;
+console.log(typeof(nome));
+
+let numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+const pessoa = "daguia";
+
+console.log(pessoa);
+
+console.log(numeroQualquer)
+
+var escopoGlobal = "global";
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = "local";
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal()
+
+let numero = "0" == 0;
+console.log(numero); */
+
+let nome = () => {
+    console.log("usando arrow function")
+}
+
+nome();
+
