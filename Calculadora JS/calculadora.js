0d44a9e032342ff71c77783b29e624a70e3a3b57
Clayton Alves <claytonjuniorsos@gmail.com>
2023-10-17 20:19:59 -0300

    update JS

--- Calculadora JS/calculadora.js
+++ Calculadora JS/calculadora.js
@@ -1,33 +1,69 @@
 function calculadora(){
     const operacao = prompt('Escolha um operação aritmética:\n 1-soma(+)\n 2-Subtração(-)\n 3-Multiplicação(*)\n 4-Divisão Real(/)\n 5-Divisão Inteira(%)\n 6-Potenciação(**)');
 
+    if (!operacao || operacao >= 7 ){
+        alert("Erro - operação inválida");
+        calculadora();
+    }else {
+
     let n1 = Number(prompt("Insira o primeiro valor: "));
     let n2 = Number(prompt("Insira o segundo valor: "));
     let resultado;
 
+    if (!n1 || !n2 ){
+        alert("Erro - parâmetros inválidos");
+        calculadora();
+    } else{
+
+    
+
     function soma(){
         resultado = n1 + n2;
-        alert(`${n1}+${n2} = ${resultado}`)
+        alert(`${n1}+${n2} = ${resultado}`);
+        novaOperacao();
     }
     function subtracao(){
         resultado = n1 - n2;
-        alert(`${n1}-${n2} = ${resultado}`)
+        alert(`${n1}-${n2} = ${resultado}`);
+        novaOperacao();
     }
     function multiplicacao(){
         resultado = n1 * n2;
-        alert(`${n1}*${n2} = ${resultado}`)
+        alert(`${n1}*${n2} = ${resultado}`);
+        novaOperacao();
     }
     function divisao_real(){
         resultado = n1 / n2;
-        alert(`${n1}/${n2} = ${resultado}`)
+        alert(`${n1}/${n2} = ${resultado}`);
+        novaOperacao();
     }
     function divisao_inteira(){
         resultado = n1 % n2;
-        alert(`${n1}%${n2} = ${resultado}`)
+        alert(`O resto da divisão de ${n1} e ${n2} é = ${resultado}`);
+        novaOperacao();
     }
     function potenciacao(){
         resultado = n1 ** n2;
-        alert(`${n1}**${n2} = ${resultado}`)
+        alert(`${n1} elevado a ${n2}ª é = ${resultado}`);
+        novaOperacao();
+    }
+
+    function novaOperacao(){
+        let opcao = prompt("Deseja fazer outra operação: \n 1=Sim \n 2=Não");
+
+        if (opcao == 1){
+            calculadora();
+
+        }else if (opcao ==2){
+            alert("Até mais!")
+        }else{
+            alert("Digite uma opção válida")
+            novaOperacao();
+
+        }
+        
+        }
+        
     }
 
 
@@ -45,6 +81,11 @@ function calculadora(){
     } else if (operacao == 6){
         potenciacao();
     }
+        
+
+    }
+
+    
 
 }
 
